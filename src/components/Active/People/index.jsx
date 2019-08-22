36d62c13a8aafed5ad967735819531cbe1f9924b
super5508 <srdev5008@gmail.com>
2019-08-22 11:42:25 -0300

    fix no contacts bug & change button position on linkedin page

--- src/components/Active/People/index.jsx
+++ src/components/Active/People/index.jsx
@@ -26,7 +26,8 @@ const PEOPLE_QUERY = gql`
 class People extends React.Component {
 
 	state = { //TODO: Using State because not sure about the working of mobx here
-		people: null
+		people: null,
+		loading: true
 	}
 	
 	
@@ -37,8 +38,15 @@ class People extends React.Component {
 		GraphQL.query(PEOPLE_QUERY)
 		.then(action((response) => {
 			console.log(`Response from peoples Query:`, response)
-			this.setState({people:response.data.catWorksDashboard})
-		})).catch(err => {
+
+			// Boolean([]) is truthly 
+			this.setState({
+				people: response.data.catWorksDashboard,
+				loading: false
+			})
+
+		}))
+		.catch(err => {
 			console.error(err)
 		})
 	}
@@ -52,18 +60,67 @@ class People extends React.Component {
 	}
 
 	render() {
-		if (!this.state.people) {
+
+		if (this.state.loading) {
+			// just a workaround to fix 'cant read focus of undefined'
 			return (
 				<section className={s.people}>
-				<Input block
-					icon='fas fa-search'
-					block
-					type='text'
-					placeholder='Search by name or company'
-					inputRef={input => this._searchInput = input}
-					onFocus={this._onSearchFocus}
-					onBlur={this._onSearchBlur}
-					onChange={this._onSearchChange} />
+					<Input block
+						icon='fas fa-search'
+						block
+						type='text'
+						placeholder='Search by name or company'
+						inputRef={input => this._searchInput = input}
+						onFocus={this._onSearchFocus}
+						onBlur={this._onSearchBlur}
+						onChange={this._onSearchChange} />
+					<Footer>
+						<Button block
+							center
+							href={config.dashboard.url}
+							target='_blank'>View on dashboard</Button>
+					</Footer>
+				</section>
+			)
+		}
+
+		if (!this.state.people.length) {
+			return (
+				<section className={s.people}>
+					<Input block
+						icon='fas fa-search'
+						block
+						type='text'
+						placeholder='Search by name or company'
+						inputRef={input => this._searchInput = input}
+						onFocus={this._onSearchFocus}
+						onBlur={this._onSearchBlur}
+						onChange={this._onSearchChange} />
+					<Footer>
+						<Button block
+							center
+							href={config.dashboard.url}
+							target='_blank'>View on dashboard</Button>
+					</Footer>
+				</section>
+			)
+		} 
+		
+		return (
+			<section className={s.people}>
+				<div  style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+						<h2>Name</h2>
+						<h2>Firm</h2>
+				</div>
+				{this.state.people.map(({ personId,	userId, first, last, company }) => {
+					// Using Inline styling because .scss wasn't working or I don't know how to use
+					return (
+						<div key={personId} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} onClick={() => this._onPersonSelect(personId)}>
+							<p>{first}{last}</p>
+							<p>{company}</p>
+						</div>
+					)
+				})}
 				<Footer>
 					<Button block
 						center
@@ -71,32 +128,7 @@ class People extends React.Component {
 						target='_blank'>View on dashboard</Button>
 				</Footer>
 			</section>
-			)
-		} if (this.state.people.length) {
-			return (
-			<section className={s.people}>
-			<div  style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-					<h2>Name</h2>
-					<h2>Firm</h2>
-			</div>
-			{this.state.people.map(({ personId,	userId, first, last, company }) => {
-				// Using Inline styling because .scss wasn't working or I don't know how to use
-				return (
-					<div key={personId} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} onClick={() => this._onPersonSelect(personId)}>
-						<p>{first}{last}</p>
-						<p>{company}</p>
-					</div>
-				)
-			})}
-			<Footer>
-				<Button block
-					center
-					href={config.dashboard.url}
-					target='_blank'>View on dashboard</Button>
-			</Footer>
-		</section>
-			)
-		}
+		)
 		
 	}
 }
