6da204c2ef824e973101a8dd24301350330e51cf
super5508 <srdev5008@gmail.com>
2019-08-29 20:07:20 -0500

    update: start event subscription after installation

--- src/background.js
+++ src/background.js
@@ -9,39 +9,46 @@ chrome.runtime.onMessage.addListener(function (message) {
 	}
 })
 
-setInterval(function() {
-  chrome.storage.local.get('activities', function({ activities }) {
-    chrome.storage.local.get('alerts', function({ alerts }) {
-      if (activities && alerts) {
-        activities.forEach(({ activity, date, person, activityId }) => {
-          const activityTime = convertDateToMinutes(date);
-          const currentTime = convertDateToMinutes(new Date());
-          const notificationBody = `${EnumActivity[activity]} with ${person.first}`;
-          const diff = activityTime - currentTime;
-          const alert = alerts.find(item => item.activityId === activityId);
+chrome.runtime.onInstalled.addListener(function() {
+  setInterval(function() {
+    chrome.storage.local.get('activities', function({ activities }) {
+      chrome.storage.local.get('alerts', function({ alerts }) {
+        if (activities && alerts) {
+          activities.forEach(({ activity, date, person, activityId }) => {
+            const activityTime = convertDateToMinutes(date);
+            const currentTime = convertDateToMinutes(new Date());
+            const notificationBody = `${EnumActivity[activity]} with ${person.first}`;
+            const diff = activityTime - currentTime;
+            const alert = alerts.find(item => item.activityId === activityId);
 
-          if (diff > 1 && diff <= 60 && !alert.isOneHourNotified) { // 1 hour
-            notifyMe('Todo Notification', {
-              body: `${notificationBody} in ${diff} minute${diff > 1 ? 's' : ''}.`,
-              requireInteraction: true,
-            });
-            chrome.storage.local.set({
-              alerts: alerts.map(item => item.activityId === activityId ? { ...item, isOneHourNotified: true } : { ...item })
-            });
-          } else if (diff > 0 && diff <= 1440 && !alert.isOneDayNotified && !alert.isOneHourNotified) { // 24 hours
-            notifyMe('Todo Notification', {
-              body: `${notificationBody} in ${Math.floor(diff / 60)} hour${Math.floor(diff / 60) > 1 ? 's' : ''}.`,
-              requireInteraction: true,
-            });
-            chrome.storage.local.set({
-              alerts: alerts.map(item => item.activityId === activityId ? { ...item, isOneDayNotified: true } : { ...item })
-            });
-          }
-        })
-      }
+            if (diff > 0 && diff <= 60 && !alert.isOneHourNotified) { // 1 hour
+              notifyMe('Todo Notification', {
+                body: `${notificationBody} in ${diff} minute${diff > 1 ? 's' : ''}.`,
+                requireInteraction: true,
+              });
+              const updatedAlerts = alerts.map(item => item.activityId === activityId
+                ? { ...item, isOneHourNotified: true, isOneDayNotified: true }
+                : { ...item }
+              );
+              chrome.storage.local.set({ alerts: updatedAlerts });
+            } else if (diff > 0 && diff <= 1440 && !alert.isOneDayNotified && !alert.isOneHourNotified) { // 24 hours
+              notifyMe('Todo Notification', {
+                body: `${notificationBody} in ${Math.floor(diff / 60)} hour${Math.floor(diff / 60) > 1 ? 's' : ''}.`,
+                requireInteraction: true,
+              });
+              const updatedAlerts = alerts.map(item => item.activityId === activityId
+                ? { ...item, isOneDayNotified: true }
+                : { ...item }
+              );
+              chrome.storage.local.set({ alerts: updatedAlerts });
+            }
+          })
+        }
+      })
     })
-  })
-}, 60000) // one minute
+  }, 60000) // one minute
+})
+
 
 function notifyMe(title, options) {
   if (!("Notification" in window)) {
